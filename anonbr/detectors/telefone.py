00cdbadb85d1679192ee922dc8316c26969a581f
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-03-07 21:42:06 -0300

    feat(telefone): TODO de classe e funções previstas para o arquivo

---
+++ anonbr/detectors/telefone.py
@@ -0,0 +1,21 @@
+"""
+Anonimizador de telefone para múltiplos formatos de telefone 
+incluindo celular e telefone fixo.
+"""
+
+import re
+from typing import Optional
+
+class DetectorTelefone:
+
+    def detectar(self, texto: str) -> list:
+
+
+    def validar()
+
+
+    def mascarar()
+
+def tetectar_telefone()
+
+def mascarar_telefone()
