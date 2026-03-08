69757929719a540d21fe50db5ac9df853f55e5df
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-03-07 21:58:20 -0300

    feat(telefone): Lista com padrões de números definida e função detectar criada.

--- anonbr/detectors/telefone.py
+++ anonbr/detectors/telefone.py
@@ -7,8 +7,43 @@ import re
 from typing import Optional
 
 class DetectorTelefone:
+    #Detecta e mascara os números de telefone
+
+    """
+    Exemplo de padrões de números: 
+    (21) 98765-4321, (21) 3456-7890, 21987654321, +55 21 98765-4321
+    """
+    padroes = [
+        r'\+55\s?\(?\d{2}\)?\s?\d{4,5}-?\d{4}', # + 55 (21) 98765-4321
+        r'\(?\d{2}\)?\s?\d{4,5}-?\d{4}', # (21) 3456-7890
+        r'\b\d{10,11}\b', # 21987654321
+    ]
+
+    def __init__(self):
+        self.regexes = [re.compile(padrao) for padrap in self.padroes]
 
     def detectar(self, texto: str) -> list:
+        """
+        Retorna lista de tuplas:
+        (telefone, posicao_inicio, posicao_fim, tipo_formato)
+        """
+        resultados = []
+        posicoes_encontradas = set()
+
+        for indice_padrao, regex in enumerate(self.regexes):
+            for match in regex.finditer(texto):
+                inicio, fim = match.start(), match.end()
+
+                # Evita sobreposição:
+                if any(i <= inicio < f or i < fim ,= f for i, f in posicoes_encontradas):
+                    continue
+
+                telefone = match.group()
+                if self._validar(telefone):
+                    resultados.append((telefone, inicio, fim, indice_padrao))
+                    posicoes_encontradas.add((inicio, fim))
+
+            return resultados
 
 
     def validar()
