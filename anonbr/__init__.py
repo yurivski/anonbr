b22ea99355add85d824203c264c5303c2673a429
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-03-08 23:13:04 -0300

    feat(anonbr/init): Transformar diretórios em pacotes (init)

---
+++ anonbr/__init__.py
@@ -0,0 +1,40 @@
+"""
+Biblioteca Python para detectar e anonimizar os dados sensíveis (CPF, email, telefone).
+
+Exemplo de uso:
+    >>> from anonbr import Anonymizer
+    >>> import pandas as pd
+    >>> 
+    >>> df = pd.read_csv('dados.csv')
+    >>> anonimizador = Anonymizer()
+    >>> df_anonimizado = anonimizador.anonimizar(df)
+
+Para mais informações: https://github.com/yurivski/anonbr
+"""
+
+__version__ = '1.0'
+__author__ = 'Yuri Pontes'
+__license__ = 'MIT'
+
+from anonbr.detectors.cpf import DetectorCPF, detectar_cpf, mascarar_cpf
+from anonbr.detectors.email import DetectorEmail, detectar_email, mascarar_email
+from anonbr.detectors.telefone import DetectorTelefone, detectar_telefone, mascarar_telefone
+
+__all__ = [
+    # Classes principais
+    'DetectorCPF',
+    'DetectorEmail',
+    'DetectorTelefone',
+    
+    # Funções auxiliares - CPF
+    'detectar_cpf',
+    'mascarar_cpf',
+    
+    # Funções auxiliares - Email
+    'detectar_email',
+    'mascarar_email',
+    
+    # Funções auxiliares - Telefone
+    'detectar_telefone',
+    'mascarar_telefone',
+]
