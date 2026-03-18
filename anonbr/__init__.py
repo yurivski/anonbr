65acbcf421aaa7fb3903679466fea9ef06e299e3
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-03-18 19:25:46 -0300

    feat: adicionar classes e funções auxiliares do PDF

--- anonbr/__init__.py
+++ anonbr/__init__.py
@@ -2,13 +2,14 @@
 Biblioteca Python para detectar e anonimizar os dados sensíveis (CPF, email, telefone).
 """
 
-__version__ = '1.0'
+__version__ = '1.5.0'
 __author__ = 'Yuri Pontes'
 __license__ = 'MIT'
 
 from anonbr.detectors.cpf import CPFDetector, detect_cpf, mask_cpf
 from anonbr.detectors.email import EmailDetector, detect_email, mask_email
 from anonbr.detectors.telefone import PhoneDetector, detect_phone, mask_phone
+from anonbr.detectors.pdf import PDFDetector, detect_pdf, mask_pdf
 from anonbr.anonymizer import Anonymizer
 
 __all__ = [
@@ -19,6 +20,7 @@ __all__ = [
     'CPFDetector',
     'EmailDetector',
     'PhoneDetector',
+    'PDFDetector',
     
     # Funções auxiliares - CPF
     'detect_cpf',
@@ -31,4 +33,8 @@ __all__ = [
     # Funções auxiliares - Telefone
     'detect_phone',
     'mask_phone',
+    
+    # Funções auxiliares - PDF
+    'detect_pdf',
+    'mask_pdf',
 ]
